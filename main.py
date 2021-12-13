5f999c5b8aa1ac97cb77e88945903037a4cd6210
ushiushi <u6822656@anu.edu.au>
2021-12-13 16:11:19 +0800

    data splitting & feature selecting added(just for test, un-formal)

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # TODO 1. --------data cleaning--------------
 import numpy as np
 import pandas as pd
+from matplotlib.pyplot import clf
+from sklearn import datasets, svm
+from sklearn.model_selection import cross_validate, train_test_split, cross_val_score
+
 df = pd.read_csv("healthcare-dataset-stroke-data.csv")
 # Determining that if missing patterns exist
 df.apply(lambda x: sum(x.isnull()), axis=0)
@@ -19,21 +23,30 @@ for index in range(0, len(df["age"])):
     df["age"] = df["age"].astype(int)
 
 # Switching the integer datatype of the column age into string
-# for index in range(0, len(df["stroke"])):
-#     if df["stroke"][index] == 1:
-#         df["stroke"][index] = "Yes"
-#     else:
-#         df["stroke"][index] = "No"
+for index in range(0, len(df["stroke"])):
+    if df["stroke"][index] == 1:
+        df["stroke"][index] = "Yes"
+    else:
+        df["stroke"][index] = "No"
 
 # Knitting the dataframe into a csv format file
-# df.to_csv("preprocessed_data.csv")
+df.to_csv("preprocessed_data.csv")
 print(df)
 
 
 # TODO 2. --------feature selection------------
+data = pd.read_csv("preprocessed_data.csv")
+divide = np.random.rand(len(data)) < 0.8
+train_data = data[divide]
+test_data = data[~divide]
+features = train_data.shape[1] - 1
 
 # TODO 3. --------split data and run model-----
+train_input = train_data.iloc[:, :features]
+train_target = train_data.iloc[:, features]
+print(train_input)
 
 # TODO 4. --------tune hyperparameters---------
+
 
 # TODO 5. --detect and then prevent overfitting-
