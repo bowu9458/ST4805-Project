4128115fddfdfcdf4db07f7c9c13ad9807ada3f8
ushiushi <u6822656@anu.edu.au>
2021-12-16 22:32:39 +0800

    data cleaning updated

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-from sklearn.linear_model import LogisticRegressionCV, LogisticRegression
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn import datasets
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import learning_curve
-import matplotlib.pyplot as plt  # 可视化模块
-
-# TODO 1. --------data cleaning--------------
 import numpy as np
 import pandas as pd
-from matplotlib.pyplot import clf
-from sklearn import datasets, svm
-from sklearn.model_selection import cross_validate, train_test_split, cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.model_selection import learning_curve
+import matplotlib.pyplot as plt
 
 
 # TODO 1. --------data cleaning--------------
@@ -20,63 +13,120 @@ def data_cleaning():
     df = df.drop(["id"], axis=1)
     # Determining that if missing patterns exist
     df.apply(lambda x: sum(x.isnull()), axis=0)
-    bmi_mean = np.mean(df["bmi"])
-    target = df["bmi"][1]
 
     # Dealing with the N/A value in the bmi column
+    list1 = []
+    list2 = []
+    list3 = []
+    list4 = []
+    list5 = []
+    list6 = []
+    list7 = []
+    list8 = []
     for index in range(0, len(df["bmi"])):
-        if str(df["bmi"][index])[0] == "n":
-            df["bmi"][index] = format(bmi_mean, '.1f')
-        else:
-            df["bmi"][index] = format(df["bmi"][index], '.1f')
+        if str(df["bmi"].iloc[index]) != "nan":
+            if df["age"].iloc[index] <= 40 and df["avg_glucose_level"].iloc[index] <= 163 and df["heart_disease"].iloc[
+                index] == 0:
+                list1.append(df["bmi"].iloc[index])
+            elif df["age"].iloc[index] <= 40 and df["avg_glucose_level"].iloc[index] > 163 and df["heart_disease"].iloc[
+                index] == 0:
+                list2.append(df["bmi"].iloc[index])
+            elif df["age"].iloc[index] <= 40 and df["avg_glucose_level"].iloc[index] <= 163 and \
+                    df["heart_disease"].iloc[index] == 1:
+                list3.append(df["bmi"].iloc[index])
+            elif df["age"].iloc[index] > 40 and df["avg_glucose_level"].iloc[index] <= 163 and df["heart_disease"].iloc[
+                index] == 0:
+                list4.append(df["bmi"].iloc[index])
+            elif df["age"].iloc[index] > 40 and df["avg_glucose_level"].iloc[index] > 163 and df["heart_disease"].iloc[
+                index] == 0:
+                list5.append(df["bmi"].iloc[index])
+            elif df["age"].iloc[index] > 40 and df["avg_glucose_level"].iloc[index] <= 163 and df["heart_disease"].iloc[
+                index] == 1:
+                list6.append(df["bmi"].iloc[index])
+            elif df["age"].iloc[index] <= 40 and df["avg_glucose_level"].iloc[index] > 163 and df["heart_disease"].iloc[
+                index] == 1:
+                list7.append(df["bmi"].iloc[index])
+            elif df["age"].iloc[index] > 40 and df["avg_glucose_level"].iloc[index] > 163 and df["heart_disease"].iloc[
+                index] == 1:
+                list8.append(df["bmi"].iloc[index])
+
+    for index in range(0, len(df["bmi"])):
+        if str(df["bmi"].iloc[index]) == "nan":
+            if df["age"].iloc[index] <= 40 and df["avg_glucose_level"].iloc[index] <= 163 and df["heart_disease"].iloc[
+                index] == 0:
+                df["bmi"].iloc[index] = np.mean(list1)
+            elif df["age"].iloc[index] <= 40 and df["avg_glucose_level"].iloc[index] > 163 and df["heart_disease"].iloc[
+                index] == 0:
+                df["bmi"].iloc[index] = np.mean(list2)
+            elif df["age"].iloc[index] <= 40 and df["avg_glucose_level"].iloc[index] <= 163 and \
+                    df["heart_disease"].iloc[index] == 1:
+                df["bmi"].iloc[index] = np.mean(list3)
+            elif df["age"].iloc[index] > 40 and df["avg_glucose_level"].iloc[index] <= 163 and df["heart_disease"].iloc[
+                index] == 0:
+                df["bmi"].iloc[index] = np.mean(list4)
+            elif df["age"].iloc[index] > 40 and df["avg_glucose_level"].iloc[index] > 163 and df["heart_disease"].iloc[
+                index] == 0:
+                df["bmi"].iloc[index] = np.mean(list5)
+            elif df["age"].iloc[index] > 40 and df["avg_glucose_level"].iloc[index] <= 163 and df["heart_disease"].iloc[
+                index] == 1:
+                df["bmi"].iloc[index] = np.mean(list6)
+            elif df["age"].iloc[index] <= 40 and df["avg_glucose_level"].iloc[index] > 163 and df["heart_disease"].iloc[
+                index] == 1:
+                df["bmi"].iloc[index] = np.mean(list7)
+            elif df["age"].iloc[index] > 40 and df["avg_glucose_level"].iloc[index] > 163 and df["heart_disease"].iloc[
+                index] == 1:
+                df["bmi"].iloc[index] = np.mean(list8)
+
+    for index in range(0, len(df["bmi"])):
+        df["bmi"].iloc[index] = format(df["bmi"].iloc[index], '.1f')
 
     # Switching the float datatype of the column age into integer
     age_mean = np.mean(df["age"])
     for index in range(0, len(df["age"])):
-        if df["age"][index] == 0:
-            df["age"][index] = age_mean
+        if df["age"].iloc[index] == 0:
+            df["age"].iloc[index] = age_mean
         df["age"] = df["age"].astype(int)
 
     # Switching the string datatype of the column gender into integer
     for index in range(0, len(df["gender"])):
-        if df["gender"][index] == "Male":
-            df["gender"][index] = 1
+        if df["gender"].iloc[index] == "Male":
+            df["gender"].iloc[index] = 1
         else:
-            df["gender"][index] = 2
+            df["gender"].iloc[index] = 2
 
     # Switching the string datatype of the column married into integer
     for index in range(0, len(df["ever_married"])):
-        if df["ever_married"][index] == "Yes":
-            df["ever_married"][index] = 1
+        if df["ever_married"].iloc[index] == "Yes":
+            df["ever_married"].iloc[index] = 1
         else:
-            df["ever_married"][index] = 0
+            df["ever_married"].iloc[index] = 0
 
     # Switching the string datatype of the column married into integer
     for index in range(0, len(df["work_type"])):
-        if df["work_type"][index] == "Private":
-            df["work_type"][index] = 1
-        elif df["work_type"][index] == "Self-employed":
-            df["work_type"][index] = 2
+        if df["work_type"].iloc[index] == "Private":
+            df["work_type"].iloc[index] = 1
+        elif df["work_type"].iloc[index] == "Self-employed":
+            df["work_type"].iloc[index] = 2
         else:
-            df["work_type"][index] = 3
+            df["work_type"].iloc[index] = 3
 
     # Switching the string datatype of the column living into integer
     for index in range(0, len(df["Residence_type"])):
-        if df["Residence_type"][index] == "Urban":
-            df["Residence_type"][index] = 1
+        if df["Residence_type"].iloc[index] == "Urban":
+            df["Residence_type"].iloc[index] = 1
         else:
-            df["Residence_type"][index] = 2
+            df["Residence_type"].iloc[index] = 2
 
     # Switching the string datatype of the column smoking status into integer
     for index in range(0, len(df["smoking_status"])):
-        if df["smoking_status"][index] == "formerly smoked":
-            df["smoking_status"][index] = 1
-        elif df["smoking_status"][index] == "smokes":
-            df["smoking_status"][index] = 2
-        elif df["smoking_status"][index] == "never smoked":
-            df["smoking_status"][index] = 3
+        if df["smoking_status"].iloc[index] == "formerly smoked":
+            df["smoking_status"].iloc[index] = 1
+        elif df["smoking_status"].iloc[index] == "smokes":
+            df["smoking_status"].iloc[index] = 2
+        elif df["smoking_status"].iloc[index] == "never smoked":
+            df["smoking_status"].iloc[index] = 3
         else:
-            df["smoking_status"][index] = 4
+            df["smoking_status"].iloc[index] = 4
 
     # Knitting the dataframe into a csv format file
     df.to_csv("preprocessed_data.csv")
@@ -94,7 +144,6 @@ def feature_selection():
     feature_score = pd.Series(feature_model.feature_importances_, index=X.columns)
     feature_score.nlargest(15).plot(kind='bar')
     plt.show()
-    df = df.drop(["gender"], axis=1)
     df = df.drop(["Residence_type"], axis=1)
     df = df.drop(["work_type"], axis=1)
     print(df)
@@ -139,7 +188,8 @@ def draw_learning_curve(df_data, df_label):
         class_weight='balanced'
     )
     train_sizes, train_score, test_score = learning_curve(logistic_regre, df_data, df_label,
-                                                          train_sizes=[0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1], cv=10,
+                                                          train_sizes=[0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8,
+                                                                       0.9, 1], cv=10,
                                                           scoring='accuracy')
     # accuracy rate
     train_score = np.mean(train_score, axis=1)
